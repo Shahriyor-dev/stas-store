ea9f6bc27c0748c9fe3f7e5eef75d7d2a5f6cda5
Шахриёр <84191677+Shahriyor-dev@users.noreply.github.com>
2022-04-18 19:01:03 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.querySelector('.size-button.size-button-1').addEventListener('click', () => {
+    document.querySelector('.size-text-1').classList.toggle('visible')
+})
+
+document.querySelector('.size-button.size-button-2').addEventListener('click', () => {
+    document.querySelector('.size-text-2').classList.toggle('visible')
+})
